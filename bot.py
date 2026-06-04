edc7ec40c41421ee27bd41816321d94b015144ba
haithamalalwi776-commits <haithamalalwi776@gmail.com>
2026-06-04 14:23:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ threading.Thread(target=run_flask, daemon=True).start()
 # ---------------------------------------------------------
 # 2. إعداد البوت الصانع الرئيسي المطور (أزرار متعددة)
 # ---------------------------------------------------------
-BOT_TOKEN="8696144716:AAGwBrwgkcR9Z5N8ftFyp6NTDLcqpDJc-rA " 
+BOT_TOKEN="8696144716:AAGwBrwgkcR9Z5N8ftFyp6NTDLcqpDJc-rA" 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 user_choices = {}
